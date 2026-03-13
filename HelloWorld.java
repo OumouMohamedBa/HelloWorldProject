c5599327585767f9ff0fa37aab5a8096a0ce7fde
OumouMohamedBa <24408@esp.mr>
2026-03-13 01:34:39 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins est tres bien!");
+        System.out.println("Hello, Jenkins est bien!");
     }
 }
